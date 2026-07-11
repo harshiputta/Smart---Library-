84bf821f8c4db37932edc5b7ccf7c939713cf62d
harshiputta <harshiputta669@gmail.com>
2026-07-11 11:49:16 +0530

    Implement scheduled job for overdue requests and reminders
    This scheduled job checks for overdue book requests and sends reminders for books due soon. It marks overdue requests and triggers corresponding events for notifications.

--- Scripts/ScheduledJobs/CheckOverdueRequests.js
+++ Scripts/ScheduledJobs/CheckOverdueRequests.js
@@ -1,1 +1,47 @@
+/**
+ * Scheduled Job: Check Overdue Requests and Send Reminders
+ * Run: Daily at 12:00 AM
+ */
+(function() {
+    gs.info("LMS: Starting CheckOverdueRequests Scheduled Job.");
 
+    var today = new GlideDate();
+    
+    // 1. MARK OVERDUE BOOKS AND TRIGGER EVENT
+    var overdueRequests = new GlideRecord('u_borrow_request');
+    overdueRequests.addQuery('u_status', 'borrowed');
+    overdueRequests.addQuery('u_due_date', '<', today);
+    overdueRequests.query();
+
+    var overdueCount = 0;
+    while (overdueRequests.next()) {
+        overdueRequests.setValue('u_status', 'overdue');
+        overdueRequests.update();
+
+        // Trigger Event for Overdue Email Notification
+        // Event registry key: u_borrow_request.overdue
+        // Parm1: Student email/sys_id, Parm2: Due date
+        gs.eventQueue('u_borrow_request.overdue', overdueRequests, overdueRequests.getValue('u_student'), overdueRequests.getValue('u_due_date'));
+        overdueCount++;
+    }
+    gs.info("LMS: Completed marking overdue requests. Total marked overdue: " + overdueCount);
+
+    // 2. TRIGGER DUE SOON REMINDERS (Books due in exactly 2 days)
+    var dueSoonDate = new GlideDate();
+    dueSoonDate.addDaysUTC(2);
+
+    var reminderRequests = new GlideRecord('u_borrow_request');
+    reminderRequests.addQuery('u_status', 'borrowed');
+    reminderRequests.addQuery('u_due_date', '=', dueSoonDate);
+    reminderRequests.query();
+
+    var reminderCount = 0;
+    while (reminderRequests.next()) {
+        // Trigger Event for Book Due Soon Reminder
+        // Event registry key: u_borrow_request.due_soon
+        gs.eventQueue('u_borrow_request.due_soon', reminderRequests, reminderRequests.getValue('u_student'), reminderRequests.getValue('u_due_date'));
+        reminderCount++;
+    }
+    gs.info("LMS: Completed sending due soon reminders. Total alerts queued: " + reminderCount);
+
+})();
